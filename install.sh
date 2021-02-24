26b2955e009a0b47bcd1cd50bc5f56bd2900f03a
Evaldo Felipe <contato@evaldofelipe.com>
2021-02-23 21:06:21 -0300

    Update brew install method

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install command-line tools using Homebrew.
 
